a512d6ea57fb03bcd43d752e913615483b8a7b79
BIT Student <classroom@bgit.rs>
2018-03-22 09:15:45 +0100

    Refresh fix

--- react/react/user_app/src/App/Home.js
+++ react/react/user_app/src/App/Home.js
@@ -52,7 +52,8 @@ class Home extends Component {
       usersService.UserFunction()
         .then((myUsers) => {
           this.setState({
-            users: myUsers
+            users: myUsers,
+            filterUsers: myUsers     
           })
         })
     }

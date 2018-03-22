71a1e92ced187b995ff11920379c395f75ffc9b5
BIT Student <classroom@bgit.rs>
2018-03-22 09:10:09 +0100

    BIT people

--- react/react/user_app/src/Entities/User.js
+++ react/react/user_app/src/Entities/User.js
@@ -4,14 +4,9 @@ class User {
         this.firstName = user.name.first;
         this.surname = user.name.last;
         this.email = user.email;
-<<<<<<< HEAD
         this.dateOfBirth = user.dob;
         this.picture = user.picture.medium;
         this.gender = user.gender
-=======
-        this.dateOfBirth = user.dob,
-        this.picture = user.picture.medium
->>>>>>> 909225afd69677b1dc1cc52da14f53851854ffe7
     }
 }
 
